518192d8d5fffdd75c51e5a12c1e05a533caa521
Masaru <hittec9609@icloud.com>
2022-11-16 15:45:00 +0900

    Add CreateTable() >> db.go

--- model/db.go
+++ model/db.go
@@ -3,6 +3,8 @@ package model
 import (
 	"fmt"
 	"os"
+
+	"gorm.io/gorm"
 )
 
 /* DBのdsnを取得する関数 */
@@ -17,4 +19,12 @@ func GetDBConfig() string {
 	// dsn: (DBの接続情報に付ける識別子)の定義
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, hostname, port, dbname) + "?charset=utf8mb4&parseTime=True&loc=Local"
 	return dsn
+}
+
+/* テーブルを作成する関数 */
+func CreateTable(db *gorm.DB) {
+	// 空き予定の開始テーブル
+	db.AutoMigrate(&StartTime{})
+	// 空き予定の終了テーブル
+	db.AutoMigrate(&EndTime{})
 }
