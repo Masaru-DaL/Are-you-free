1e0b623e5b671ff753095e7d3bf8dcae4a734fdf
Masaru <hittec9609@icloud.com>
2022-11-18 14:02:43 +0900

    Fix main.go Apply GET Method

--- cmd/schedule/main.go
+++ cmd/schedule/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"Are-you-free/internal/controllers"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
+
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!!")
+		return c.String(http.StatusCreated, "Hello, World!!")
 	})
+	e.GET("/schedules", controllers.GetSchedules)
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
