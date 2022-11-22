7c977c0d35eda48bcdca2f7b2553d4e96ebd39ad
Masaru <hittec9609@gmail.com>
2022-11-22 09:41:29 +0900

    html/templateでURLにパラメータを渡した値の表示のテスト

--- internal/controllers/schedule.go
+++ internal/controllers/schedule.go
@@ -19,5 +19,6 @@ func GetSchedules(c echo.Context) error {
 
 // template
 func Hello(c echo.Context) error {
-	return c.Render(http.StatusOK, "hello", "World")
+	data := c.QueryParam("id")
+	return c.Render(http.StatusOK, "hello", data)
 }
