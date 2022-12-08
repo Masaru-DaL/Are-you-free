0fc1d85fb3be27aa241de79a34db780c431b72ee
Masaru <hittec9609@gmail.com>
2022-12-08 13:41:49 +0900

    [fix] テーブル初期データ投入

--- src/mysql/init/are-you-free.sql
+++ src/mysql/init/are-you-free.sql
@@ -1,3 +1,4 @@
+-- users table
 create table if not exists users(
     id int(10)  AUTO_INCREMENT NOT NULL primary key,
     name CHAR(20) NOT NULL,
@@ -5,4 +6,24 @@ create table if not exists users(
     is_admin BOOLEAN NOT NULL DEFAULT 0,
     created_at DATETIME DEFAULT current_timestamp,
     updated_at DATETIME DEFAULT current_timestamp
-)
+);
+
+-- schedules table
+create table if not exists schedules(
+    id int(10)  AUTO_INCREMENT NOT NULL primary key,
+    year INTEGER NOT NULL,
+    month INTEGER NOT NULL,
+    day INTEGER NOT NULL,
+    starthour INTEGER NOT NULL,
+    startminute INTEGER NOT NULL,
+    endhour INTEGER NOT NULL,
+    endminute INTEGER NOT NULL,
+    created_at DATETIME DEFAULT current_timestamp,
+    updated_at DATETIME DEFAULT current_timestamp
+);
+
+insert into schedules (year, month, day, starthour, startminute, endhour, endminute) values (2022, 11, 18, 18, 00, 24, 00);
+insert into schedules (year, month, day, starthour, startminute, endhour, endminute) values (2022, 11, 20, 10, 00, 12, 00);
+
+insert into users (name, password) values ("hoge", "hoge");
+insert into users (name, password) values ("fuga", "fuga");
